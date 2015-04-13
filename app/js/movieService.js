95969f48e5e5cf97d7bfb8c81a29e1f7176ebdd4
mfrisell <mfrisell@kth.se>
2015-04-13 16:22:07 +0200

    progress

--- app/js/movieService.js
+++ app/js/movieService.js
@@ -1,8 +1,8 @@
 
 movieDataApp.factory('Movie',function ($resource) {
 
+this.fetchPop = $resource('https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&append_to_response=trailers',{api_key:'33e53562fbe46873e9379ecef2545dbc'});
 
 
-  return this;
-
+return this;
 });
--- app/js/startCtrl.js
+++ app/js/startCtrl.js
@@ -1,9 +1,8 @@
 movieDataApp.controller('StartCtrl', function ($scope,Movie) {
 
-$scope.test = function() {
-//var url = 'https://api.themoviedb.org/3/search/movie?query=';
-var url = 'https://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc';
-		userInp = "superman";
+angular.element(document).ready(function () {
+	var url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&append_to_response=trailers';
+	//var url = 'https://api.themoviedb.org/3/movie/157336/videos';
         key = '&api_key=33e53562fbe46873e9379ecef2545dbc';
         $.ajax({
             type: 'GET',
@@ -14,9 +13,34 @@ var url = 'https://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc
             contentType: 'application/json',
             dataType: 'jsonp',
             success: function(json) {
-				alert("NÖJJS");
-                console.dir(json);
+				console.log(json);
+                //console.dir(json.results[0]);
+				//Movie.updatePopMovie(json.results[0])
+				imageLink = "https://image.tmdb.org/t/p/w1920" + json.results[0].backdrop_path;
+				$(".contentImage").css("background-image","url(" + imageLink + ")");
+				$scope.getVideos(json.results[0].id);
+				
             },
+            error: function(e) {
+				alert("RÖV");
+                console.log(e.message);
+            }
+    });
+});
+
+$scope.getVideos = function(videoId) {
+	var url = 'https://api.themoviedb.org/3/movie/' + videoId + '/videos';
+        key = '?api_key=33e53562fbe46873e9379ecef2545dbc';
+        $.ajax({
+            type: 'GET',
+			url: url + key,
+            async: false,
+            jsonpCallback: 'testing',
+            contentType: 'application/json',
+            dataType: 'jsonp',
+            success: function(json) {
+				console.log(json);
+			},
             error: function(e) {
 				alert("RÖV");
                 console.log(e.message);
