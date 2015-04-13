e756f757022a760deea87932fadd50cedb178f6f
gusnik <gusnik@gmail.com>
2015-04-13 14:50:06 +0200

    FIxat search till objekt

--- app/js/movieService.js
+++ app/js/movieService.js
@@ -1,5 +1,29 @@
 
-movieDataApp.factory('Movie',function ($resource, $cookieStore) {
+movieDataApp.factory('Movie',function ($resource) {
+
+
+this.getMovieByStr = function(userinput) {
+var objectList = [];
+var url = 'https://api.themoviedb.org/3/search/movie?query=';
+		userInp = userinput;
+        key = '&api_key=33e53562fbe46873e9379ecef2545dbc';
+        $.ajax({
+            type: 'GET',
+            url: url + userInp + key,
+            async: false,
+            jsonpCallback: 'testing',
+            contentType: 'application/json',
+            dataType: 'jsonp',
+            success: function(json) {
+                console.dir(json);
+                objectList.push(json.results);
+            },
+            error: function(e) {
+                console.log(e.message);
+            }
+    });
+  return (objectList);
+}
 
 
 
--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -1,6 +1,12 @@
 
 movieDataApp.controller('SearchCtrl', function ($scope,Movie) {
 
+$scope.getSearchResults = function(){
+	var valu = $("#selectKeyWords").val();
+	objectList = Movie.getMovieByStr(valu);
+	console.log(objectList);
+
+}
 
 
 
