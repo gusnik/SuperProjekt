b81163600e7b8f742591735f768122f407925e8a
mfrisell <mfrisell@kth.se>
2015-04-13 14:23:26 +0200

    discover function

--- app/js/startCtrl.js
+++ app/js/startCtrl.js
@@ -2,13 +2,13 @@ movieDataApp.controller('StartCtrl', function ($scope,Movie) {
 
 $scope.test = function() {
 //var url = 'https://api.themoviedb.org/3/search/movie?query=';
-var url = 'https://api.themoviedb.org/3/search/movie?query=';
-
+var url = 'https://api.themoviedb.org/3/discover/movie?sort_by=vote_average.desc';
 		userInp = "superman";
         key = '&api_key=33e53562fbe46873e9379ecef2545dbc';
         $.ajax({
             type: 'GET',
-            url: url + userInp + key,
+            //url: url + userInp + key,
+			url: url + key,
             async: false,
             jsonpCallback: 'testing',
             contentType: 'application/json',
