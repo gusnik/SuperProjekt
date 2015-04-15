322f8f4839a29f5ec0fc693ecd1c5d4eb979c40b
mfrisell <mfrisell@kth.se>
2015-04-15 12:15:25 +0200

    niklas search included (not working yet)

--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -1,3 +1,52 @@
+
+movieDataApp.controller('SearchCtrl', function ($scope,Movie,$http) {
+
+$scope.getList = function() {
+	return Movie.getObjectlist();
+	$scope.movieList = Movie.getObjectlist();
+
+}
+$scope.getResults = function() {
+	 $scope.$apply();
+
+
+}
+
+
+$scope.getSearchResults = function() {
+var valu = $("#selectKeyWords").val();
+var url = 'https://api.themoviedb.org/3/search/movie?query=';
+		userInp = valu;
+        key = '&api_key=33e53562fbe46873e9379ecef2545dbc';
+        $.ajax({
+            type: 'GET',
+            url: url + userInp + key,
+            async: false,
+            jsonpCallback: 'testing',
+            contentType: 'application/json',
+            dataType: 'jsonp',
+            success: function(json) {
+                console.dir(json);
+                $scope.movies = json.results;
+                console.log($scope.movies);
+                $scope.getResults();
+
+
+
+            },
+            error: function(e) {
+                console.log(e.message);
+            }
+    });
+
+}
+
+
+
+
+
+
+});
 movieDataApp.controller('SearchCtrl', function ($scope,Movie) {
 
 
