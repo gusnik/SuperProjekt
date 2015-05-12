e219ead5c192b1e9cad8057af9e3f2fb32ec54bc
gusnik <gusnik@gmail.com>
2015-05-12 13:37:00 +0200

    More fixing

--- app/js/movieCtrl.js
+++ app/js/movieCtrl.js
@@ -1,6 +1,6 @@
 movieDataApp.controller('MovieCtrl', function ($scope,$routeParams,Movie) {
 
-$scope.ratingValues = [1, 2, 3, 4, 5];
+$scope.ratingValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 $scope.currentMovie = '';
 
@@ -13,6 +13,7 @@ $scope.getResults = function() {
 $scope.startPartial = function() {
     $scope.getMovieByID();
 }
+
 
 
 
