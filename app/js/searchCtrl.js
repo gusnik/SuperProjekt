959d47af70374b995b1a1b61d504be74c3e1af59
gusnik <gusnik@gmail.com>
2015-05-12 13:30:49 +0200

    Fixing branchs

--- app/js/searchCtrl.js
+++ app/js/searchCtrl.js
@@ -6,71 +6,25 @@ $scope.getList = function() {
 	$scope.movieList = Movie.getObjectlist();
 
 }
+
+
 $scope.getResults = function() {
-	 $scope.$apply();
+     if(!$scope.$$phase) {
+             $scope.$apply();
+
+    }
 }
-
-
-    $scope.getTop = function() {
-        var valu = $("#topBtn").val();
-        var url = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc'
-        key = '&api_key=33e53562fbe46873e9379ecef2545dbc';
-        $.ajax({
-            type: 'GET',
-            url: url +  key,
-            async: false,
-            jsonpCallback: 'testing',
-            contentType: 'application/json',
-            dataType: 'jsonp',
-            success: function(json) {
-                console.dir(json);
-                $scope.topMovies = json.results;
-                console.log($scope.topMovies);
-                $scope.getResults();
-
-
-
-    },
-        error: function(e) {
-            console.log(e.message);
-        }
-    });
-
-}
-
 
 
 $scope.getSearchResults = function() {
-var valu = $("#selectKeyWords").val();
-var url = 'https://api.themoviedb.org/3/search/movie?query=';
-		userInp = valu;
-        key = '&api_key=33e53562fbe46873e9379ecef2545dbc';
-        $.ajax({
-            type: 'GET',
-            url: url + userInp + key,
-            async: false,
-            jsonpCallback: 'testing',
-            contentType: 'application/json',
-            dataType: 'jsonp',
-            success: function(json) {
-                console.dir(json);
-                $scope.movies = json.results;
-                console.log($scope.movies);
-                $scope.getResults();
-
-
-
-            },
-            error: function(e) {
-                console.log(e.message);
-            }
-    });
-
+    var valu = $("#selectKeyWords").val();
+    Movie.getMovies.getSearch({query:valu}, function(data){
+        $scope.movies = data.results;
+        $scope.getResults();
+        },function(data){
+     $scope.status = "There was an error";
+});
 }
-
-
-
-
 
 
 });
